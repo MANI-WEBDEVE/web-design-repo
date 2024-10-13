e5d5daa9a61884d0c03c4755bdfc08b4d5e95eeb
Muhammad Inam <nudmaufon@gmail.com>
2024-10-13 22:44:24 +0500

    web-design 3

---
+++ design-no3/src/app.js
@@ -0,0 +1,37 @@
+gsap.from(".text-div h1", {
+    x:-100,
+    duration:2,
+    delay:1,
+    opacity:0,
+    stagger:0.5
+}) 
+gsap.from(".part2", {
+    scale:0,
+    duration:2,
+    delay:1,
+    opacity:0,
+    stagger:0.5
+})
+gsap.from(".smallImage", {
+    x:100,
+    duration:2,
+    delay:1,
+    opacity:0,
+    stagger:0.5
+
+})
+gsap.from('.verticalText', {
+    top:'0%',
+    duration:2,
+    delay:3,
+    opacity:0,
+    stagger:0.5
+})
+gsap.from(".nav", {
+    top:"26%",
+    duration:4,
+    delay:1,
+    opacity:0,
+    stagger:0.5
+
+})
